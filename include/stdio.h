d74f94ac65f3fd293538bba9cefd13a6131bf436
bozidarsk <bozidarsk@proton.me>
2026-03-19 19:06:36 +0200

    Added dprintf.

--- include/stdio.h
+++ include/stdio.h
@@ -6,6 +6,7 @@
 ssize_t write(int fd, const void* buffer, size_t size);
 
 void printf(const char* format, ...);
+void dprintf(int fd, const char* format, ...);
 void sprintf(char* buffer, const char* format, ...);
 void snprintf(char* buffer, size_t size, const char* format, ...);
 
