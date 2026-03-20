513c26c280332011b00f596acde5a5ef0c86b335
bozidarsk <bozidarsk@proton.me>
2026-03-20 20:51:44 +0200

    MADT parsing.

--- src/kernel/acpi.c
+++ src/kernel/acpi.c
@@ -1,3 +1,35 @@
+#include <stddef.h>
+#include <assert.h>
+
 #include "kernel/acpi.h"
 
+void acpi_enumerate_xsdt(const XSDT* xsdt, void(*callback)(SDT* header)) 
+{
+	assert(xsdt);
+	assert(callback);
 
+	assert(xsdt->header.type == SDT_TYPE_XSDT);
+
+	int entries = (xsdt->header.size - sizeof(xsdt->header)) / sizeof(xsdt->pOtherHeaders[0]);
+
+	for (int i = 0; i < entries; i++) 
+	{
+		SDT* header = (SDT*)xsdt->pOtherHeaders[i];
+		callback(header);
+	}
+}
+
+void acpi_enumerate_madt(const MADT* madt, void(*callback)(MADTEntry* entry)) 
+{
+	assert(madt);
+	assert(callback);
+
+	uint32_t max = madt->header.size - sizeof(MADT);
+	MADTEntry* entry = NULL;
+
+	for (uint32_t i = 0; i < max; i += entry->size) 
+	{
+		entry = (MADTEntry*)(madt->entries + i);
+		callback(entry);
+	}
+}
